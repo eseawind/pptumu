914017cb22a8f70ac20918ba5d4d9a3416f825e6
Weiguang.Yin <wgyin@deb.wgyin>
2015-01-13 00:38:13 +0800

    init

---
+++ tmp/log/php.20150111.log.php
@@ -0,0 +1,15 @@
+<?php
+ die();
+?>
+
+17:47:20 Undefined property: config::$debug in /var/apache/htdocs/zentaopms.lo/module/common/view/header.lite.html.php on line 19 when visiting /install.php
+
+17:47:33 Undefined property: config::$debug in /var/apache/htdocs/zentaopms.lo/module/common/view/header.lite.html.php on line 19 when visiting /install.php?m=install&f=step1
+
+17:47:43 Undefined property: config::$debug in /var/apache/htdocs/zentaopms.lo/module/common/view/header.lite.html.php on line 19 when visiting /install.php?m=install&f=step2
+
+17:47:43 Undefined property: config::$debug in /var/apache/htdocs/zentaopms.lo/module/common/view/header.lite.html.php on line 19 when visiting /install.php
+
+17:49:44 Undefined property: config::$debug in /var/apache/htdocs/zentaopms.lo/module/common/view/header.lite.html.php on line 19 when visiting /install.php?m=install&f=step3
+
+17:50:26 Creating default object from empty value in /var/apache/htdocs/zentaopms.lo/module/install/model.php on line 440 when visiting /install.php?m=install&f=step4
